1181aadf677e5f6083816744f0bce9ce409a1c76
Subin <subinsamrat96@gmail.com>
2020-10-31 22:30:01 +0530

    react hooks todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Todo from "./todo";
+import TodoForm from "./TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Upload the blog posts before tomorrow",
+      hasCompleted: false,
+    },
+
+    {
+      text: "Go to shop and buy shampoo",
+      hasCompleted: false,
+    },
+
+    {
+      text: "Finish Season 3 of Flash",
+      hasCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].hasCompleted = !newTodos[index].hasCompleted;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        <h1>TODO LIST</h1>
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => {
+          return (
+            <Todo
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
